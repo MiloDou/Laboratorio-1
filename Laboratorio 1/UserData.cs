4b5309970c236600cce32fab9e865b15170ab4ab
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-29 08:42:39 -0600

    Creacion de las clases

---
+++ Laboratorio 1/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1
+{
+    public class UserData
+    {
+        List<string> users = new List<string>();
+
+    }
+}
